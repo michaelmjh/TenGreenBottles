40cd2e67ee99a2f8ae566e2ff0d9c366be35b1ea
Michael Hughes <michaelmjh@icloud.com>
2021-01-24 19:44:59 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TenGreenBottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int remainingBottles = 10;
+            string pluralS = "s";
+
+            while (remainingBottles > 0)
+            {
+                Console.WriteLine($"{remainingBottles} green bottle{pluralS} standing on the wall");
+                remainingBottles--;
+                if (remainingBottles == 1)
+                {
+                    pluralS = "";
+                }
+            }
+            
+        }
+    }
+}
