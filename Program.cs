ecb4984b2212152bbce16f2a6f5f3617ac65aad8
Michael Hughes <michaelmjh@icloud.com>
2021-02-08 19:39:38 +0000

    updates

--- Program.cs
+++ Program.cs
@@ -6,19 +6,25 @@ namespace TenGreenBottles
     {
         static void Main(string[] args)
         {
-            int remainingBottles = 10;
+            int remainingBottles = 5;
             string pluralS = "s";
 
             while (remainingBottles > 0)
             {
                 Console.WriteLine($"{remainingBottles} green bottle{pluralS} standing on the wall");
+                Console.WriteLine($"and if 1 green bootle should accidently fall");
                 remainingBottles--;
                 if (remainingBottles == 1)
                 {
                     pluralS = "";
                 }
+                else
+                {
+                    pluralS = "s";
+                }
+                Console.WriteLine($"there will be {remainingBottles} green bottle{pluralS} standing on the wall");
+                Console.WriteLine();
             }
-            
         }
     }
 }
